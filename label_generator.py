1899352bc48d43d6daa0b6adcebfd445aa44fdfe
hellimax <harjindersinghs168@gmail.com>
2021-05-27 01:51:29 +0530

    adding label generator

---
+++ label_generator.py
@@ -0,0 +1,52 @@
+import os
+import cv2
+import json
+import numpy as np
+
+def generate_labels():
+    
+    img_list = list(os.walk("./train_data"))[0][2]
+    max_len_image = max(list(os.walk("./train_data/"))[0][2],key=len)
+    max_char_len = len(max_len_image.split("_"))-1
+    max_len_image = cv2.imread("./train_data/"+max_len_image).shape[1]
+
+    blank = cv2.imread("./blank.png")
+    train_len = int((len(img_list)*80)/100)
+    with open("./Att_ocr/data/index_to_char.json","r") as f:
+        index_to_char = json.load(f)
+    
+    print(len(img_list))
+    
+
+    with open("train_labels.txt","w") as file:
+        for i in range(0,train_len):
+            img_name = img_list[i].split("_")
+            img = cv2.imread("./train_data/"+img_list[i])
+            while img.shape[1]!=max_len_image:
+                img = np.concatenate((img,blank),axis=1)
+            label = ""
+            for j in range(len(img_name)-1):
+                label = label+index_to_char[img_name[j]]
+            if len(img_name)-1<max_char_len:
+                label = label+(max_char_len-(len(img_name)-1))*"@"
+            
+            file.write("./train_data/"+img_list[i]+" "+label+"\n")
+
+    with open("test_labels.txt","w") as file:
+        for i in range(train_len,len(img_list)):
+            img_name = img_list[i].split("_")
+            img = cv2.imread("./train_data/"+img_list[i])
+            while img.shape[1]!=max_len_image:
+                img = np.concatenate((img,blank),axis=1)
+            label = ""
+            for j in range(len(img_name)-1):
+                label = label+index_to_char[img_name[j]]
+            if len(img_name)-1<max_char_len:
+                label = label+(max_char_len-(len(img_name)-1))*"@"
+            
+            file.write("./train_data/"+img_list[i]+" "+label+"\n")
+
+
+
+if __name__=="__main__":
+    generate_labels()
